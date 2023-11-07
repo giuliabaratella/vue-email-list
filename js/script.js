f111e4650f385410c38479f61c65e0ca43caa1d6
giuliabaratella <giuliabaratella7@gmail.com>
2023-11-07 15:23:00 +0100

    added btn to gen new list

--- js/script.js
+++ js/script.js
@@ -7,16 +7,19 @@ createApp({
         }
     },
     methods:{
-
+        newList(){
+            this.emails = [];
+            for(let i = 0; i < 10; i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((resp)=>{
+                    // console.log(resp.data.response);
+                    this.emails.push(resp.data.response);
+        
+                })
+            }
+        }
     },
     mounted(){
-        for(let i = 0; i < 10; i++){
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((resp)=>{
-                // console.log(resp.data.response);
-                this.emails.push(resp.data.response);
-    
-            })
-        }
+        this.newList();
 
     }
 }).mount('#app')
