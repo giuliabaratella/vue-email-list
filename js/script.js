20d8e0894bb3189413efbb733c9c7fba2a6ccef7
giuliabaratella <giuliabaratella7@gmail.com>
2023-11-07 14:57:09 +0100

    used axios library + got data from API and connected to html

--- js/script.js
+++ js/script.js
@@ -3,13 +3,17 @@ const {createApp} = Vue;
 createApp({
     data(){
         return{
-            
+            emails:[]
         }
     },
     methods:{
 
     },
     mounted(){
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((resp)=>{
+            // console.log(resp.data.response);
+            this.emails.push(resp.data.response);
 
+        })
     }
 }).mount('#app')
