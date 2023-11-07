0c72bc26dc6a9500fcd9c24033358dabc516fc91
giuliabaratella <giuliabaratella7@gmail.com>
2023-11-07 15:14:06 +0100

    added style + fixed loop + bonus

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ createApp({
 
     },
     mounted(){
-        for(let i = 0; i <= 10; i++){
+        for(let i = 0; i < 10; i++){
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((resp)=>{
                 // console.log(resp.data.response);
                 this.emails.push(resp.data.response);
