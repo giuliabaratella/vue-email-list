faeeb0b8cc3566ff3400662fc41fc364cef52287
giuliabaratella <giuliabaratella7@gmail.com>
2023-11-07 14:59:34 +0100

    added for loop to print the list

--- js/script.js
+++ js/script.js
@@ -10,10 +10,13 @@ createApp({
 
     },
     mounted(){
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((resp)=>{
-            // console.log(resp.data.response);
-            this.emails.push(resp.data.response);
+        for(let i = 0; i <= 10; i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((resp)=>{
+                // console.log(resp.data.response);
+                this.emails.push(resp.data.response);
+    
+            })
+        }
 
-        })
     }
 }).mount('#app')
